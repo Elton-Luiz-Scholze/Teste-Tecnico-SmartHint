bccd60a26712b222fc003ae4ee826ddacbff6936
Elton Luiz Scholze <elscholze@gmail.com>
2023-07-13 22:11:35 -0300

    fix: alter views

---
+++ Teste-Tecnico-SmartHint/Teste-Tecnico-SmartHint/Migrations/20230714005313_AlterStateRegistrationColumnSizeAndRegex.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Teste_Tecnico_SmartHint.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterStateRegistrationColumnSizeAndRegex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StateRegistration",
+                table: "Customers",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(12)",
+                oldMaxLength: 12)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StateRegistration",
+                table: "Customers",
+                type: "varchar(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldMaxLength: 15)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
